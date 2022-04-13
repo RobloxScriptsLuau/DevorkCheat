fed5fb27d951cdaf6b8e38ff80a0e37b909f9b43
Devork <88288729+Devorkk@users.noreply.github.com>
2022-04-13 15:04:45 +0200

    Update Devork Menu 1.6.user.js

--- Devork Menu 1.6.user.js
+++ Devork Menu 1.6.user.js
@@ -3,7 +3,7 @@
 // @run-at               document-start
 // @version              1.6
 // @author               Devork
-// @namespace            https://github.com/Devoxxo/
+// @namespace            https://github.com/Devorkk/
 // @match                https://www.kogama.com/page/webgl-frame/*
 // @match                https://kogama.com.br/page/webgl-frame/*
 // @match                https://friends.kogama.com/page/webgl-frame/*
