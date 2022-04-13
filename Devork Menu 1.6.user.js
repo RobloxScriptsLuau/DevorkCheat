1bb6d17a74340ece1fbcb27a2e0b0e10ffaf475e
Devoxx <88288729+Devoxxo@users.noreply.github.com>
2022-04-13 13:22:06 +0200

    Create Devork Menu 1.6.user.js

---
+++ Devork Menu 1.6.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name                 Devork Menu [BETA]
+// @run-at               document-start
+// @version              1.6
+// @author               Devork
+// @namespace            https://github.com/Devoxxo/
+// @match                https://www.kogama.com/page/webgl-frame/*
+// @match                https://kogama.com.br/page/webgl-frame/*
+// @match                https://friends.kogama.com/page/webgl-frame/*
+// @require              https://pastebin.com/raw/wY1cfgMQ/
+// ==/UserScript==
