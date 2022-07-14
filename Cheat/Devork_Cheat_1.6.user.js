ff3c99e26dbee92caadc5aef102b057dbafe7fb9
Devork <88288729+Devorkk@users.noreply.github.com>
2022-07-14 15:28:39 +0200

    Create Devork_Cheat_1.6.user.js

---
+++ Cheat/Devork_Cheat_1.6.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name                 Devork Menu [BETA]
+// @run-at               document-start
+// @version              1.6
+// @author               Devork
+// @namespace            https://github.com/Devorkk/
+// @match                https://www.kogama.com/page/webgl-frame/*
+// @match                https://kogama.com.br/page/webgl-frame/*
+// @match                https://friends.kogama.com/page/webgl-frame/*
+// @require              
+// @grant                none
+// ==/UserScript==
