dae17de3a32163a21e7e546b31aa5258e74cac5e
Devork <88288729+Devorkk@users.noreply.github.com>
2022-11-30 23:11:07 +0100

    Create Devork-Cheat-v1.7.js

---
+++ Versions/v1.7/Devork-Cheat-v1.7.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name                 Devork Cheat
+// @version              1.7
+// @author               Devork
+// @match                https://www.kogama.com/page/webgl-frame/*
+// @match                https://kogama.com.br/page/webgl-frame/*
+// @match                https://friends.kogama.com/page/webgl-frame/*
+// @require              https://raw.githubusercontent.com/Devorkk/DevorkCheat/main/Versions/v1.7/Devork-Cheat-Source-v1.7.js
+// @run-at               document-start
+// @grant                none
+// ==/UserScript==
