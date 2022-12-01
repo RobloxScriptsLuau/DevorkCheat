40f63155b643a0a3c53fec9a27f88fa7517e3897
Devork <88288729+Devorkk@users.noreply.github.com>
2022-12-01 17:48:49 +0100

    Update and rename Versions/v1.7/Devork-Cheat-v1.7.user.js to versions/v1.7/Devork-Cheat-v1.7.user.js

--- versions/v1.7/Devork-Cheat-v1.7.user.js
+++ versions/v1.7/Devork-Cheat-v1.7.user.js
@@ -5,7 +5,7 @@
 // @match                https://www.kogama.com/page/webgl-frame/*
 // @match                https://kogama.com.br/page/webgl-frame/*
 // @match                https://friends.kogama.com/page/webgl-frame/*
-// @require              https://raw.githubusercontent.com/Devorkk/DevorkCheat/main/Versions/v1.7/Devork-Cheat-Source-v1.7.js
+// @require              https://raw.githubusercontent.com/Devorkk/DevorkCheat/main/versions/v1.7/Devork-Cheat-Source-v1.7.js
 // @run-at               document-start
 // @grant                none
 // ==/UserScript==
