e62c1b20be5a32c85fc8dd1565cb2fcf5b5df41c
RobloxScriptsLuau <DividedBy1000@proton.me>
2025-01-17 18:49:54 +0200

    Update Devork-Cheat-v1.7.user.js

--- versions/v1.7/Devork-Cheat-v1.7.user.js
+++ versions/v1.7/Devork-Cheat-v1.7.user.js
@@ -2,11 +2,11 @@
 // @name                 Devork Cheat
 // @version              1.7
 // @author               Devork
-// @namespace            https://github.com/Devorkk/
-// @match                https://www.kogama.com/page/webgl-frame/*
-// @match                https://kogama.com.br/page/webgl-frame/*
-// @match                https://friends.kogama.com/page/webgl-frame/*
-// @require              https://raw.githubusercontent.com/Devorkk/DevorkCheat/main/versions/v1.7/Devork-Cheat-Source-v1.7.js
+// @namespace            https://github.com/RobloxScriptsLuau/
+// @match                https://www.kogama.com/
+// @match                https://kogama.com.br/
+// @match                https://friends.kogama.com/
+// @require              https://raw.githubusercontent.com/D=RobloxScriptsLuau/DevorkCheat/main/versions/v1.7/Devork-Cheat-Source-v1.7.js
 // @run-at               document-start
 // @grant                none
 // ==/UserScript==
